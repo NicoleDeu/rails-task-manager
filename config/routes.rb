fb83fa9065c0a49d438d9c130c42bbbca671489b
Nicole Deutrich <nicoledeutrich@t-online.de>
2023-08-15 15:48:28 +0200

    Add CRUD actions: CR

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#index"
+  get "task/new", to: "tasks#new", as: :new_task
+  post "tasks", to: "tasks#create"
+  get "task/:id", to: "tasks#show", as: :task
 end
